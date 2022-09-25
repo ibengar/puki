b9eb03b167cb8a890c87f0e78238f40f2fa9b522
ibengar <56824876+ibengar@users.noreply.github.com>
2022-09-25 21:29:48 +0800

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -21,11 +21,11 @@ if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/
 *Nickname:* ${nickname}
 *Description:* ${description}
 
-_©WH-MODS-BOT-V1🍭_
+_©.iBeng_
 `.trim(), m)
 }
-handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
+handler.help = ['tiktok', 'tt', 'tiktoknowm'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
+handler.command = /^(tik(tt(tok)?(tok)?(dl)?)$/i
 
 export default handler
