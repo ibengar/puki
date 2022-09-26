0d43571aa0dda9ca14af8b1347d52988d6263724
ibengar <56824876+ibengar@users.noreply.github.com>
2022-09-27 07:32:40 +0800

    Update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -5,7 +5,7 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
-    let teks = `「 WH-MODS-BOT-V1 」
+    let teks = ` iBeng-Botz
 
     ╭━━━━「 SEWA 」
     ┊⫹⫺ Hemat: 5k/grup (1 minggu)
@@ -24,18 +24,18 @@ handler.all = async function (m, { isBlocked }) {
     ╰═┅═━––––––๑
     
     ⫹⫺ PAYMENT:
-    • Pulsa Telkomsel: [082127487538]
-    • Dana: [082127487538]
-    • Gopay: [082127487538]
-    • Ovo: [082127487538]
-    • Link Aja: [082127487538]
+    • Pulsa Telkomsel: [081257172080]
+    • Dana: [081257172080]
+    • Gopay: [081257172080]
+    • Ovo: [081257172080]
+    • Link Aja: [081257172080]
     
     Nomor Owner :
-    wa.me/6282127487538
+    wa.me/6281257172080
     
     ▌│█║▌║▌║║▌║▌║█│▌
     
-    #BY WH MODS DEV`
+    #BY iBeng`
     this.reply(m.chat, teks, m)
     const data = global.owner.filter(([id, isCreator]) => id && isCreator)
     this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
