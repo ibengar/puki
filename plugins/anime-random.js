cdbd9d800b881473fc8f3483a8630862d94294c8
ibengar <56824876+ibengar@users.noreply.github.com>
2022-10-01 10:07:52 +0800

    Update anime-random.js

--- plugins/anime-random.js
+++ plugins/anime-random.js
@@ -232,7 +232,7 @@ conn.sendButton(m.chat, `_${command}_`.trim(), author, haha.data, [['ðŸ”„ NEXT ð
 }
 handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay']
 handler.tags = ['anime']
-export default handler/*
+export default handler*/
 
 
 
