5875c9f1e8a2e8515733fde31573ed2fa4d8577f
ibengar <56824876+ibengar@users.noreply.github.com>
2022-10-01 10:01:55 +0800

    Update anime-jahy.js

--- plugins/anime-jahy.js
+++ plugins/anime-jahy.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://api.xteam.xyz/randomimage/jahy?APIKEY=Apikeymu'
+	let url = 'https://rest-api-ibeng.herokuapp.com/api/nsfw/jahy?apikey=ibeng'
 	conn.sendButton(m.chat, 'Wangy wangy (≧ω≦)', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
 handler.command = /^(jahy)$/i
